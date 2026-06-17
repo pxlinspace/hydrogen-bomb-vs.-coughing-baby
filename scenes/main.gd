d7436be0c40776656bc456043925a50a3b8839c7
pxlinspace <108901892+pxlinspace@users.noreply.github.com>
2026-06-17 02:19:25 -0700

    added digital timer

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,11 +2,15 @@ extends Node
 
 @onready var restart_timer: Timer = $RestartTimer
 @onready var restart_bar: TextureProgressBar = $GUI/HUD/RestartBar
+@onready var bomb_timer: Timer = $BombTimer
+@onready var bomb_time_label: Label = $GUI/HUD/BombTimeLabel
 
 
 func _process(_delta: float) -> void:
 	if not restart_timer.is_stopped():
 		restart_bar.value = 1.0 - restart_timer.time_left / restart_timer.wait_time
+	var cs: float = bomb_timer.time_left * 100.0
+	bomb_time_label.text = "%02d:%02d:%02d" % [fmod(cs/6000, 60), fmod(cs/100, 60), fmod(cs, 100)]
 
 
 func _input(event: InputEvent) -> void:
